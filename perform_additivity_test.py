52efa63d9a8544154d0c094c2afb38fa309bcd7a
Alexander Serov <asserov@gmail.com>
2019-09-11 10:58:43 +0200

    Put Welch's test in a separate function.

--- perform_additivity_test.py
+++ perform_additivity_test.py
@@ -1,6 +1,8 @@
 """
 Perform Welch's unequal variances t-test to evaluate whenther the additivity hypothesis can be refuted for the given data sets.
 The code calculates the characteristics of the random variable equal to the sum of values of the no primary and no shadow constructs and compares it to the distribution of bac.
+
+Another part of the code checks whether the bac value is the same across different constructs.
 """
 
 import numpy as np
@@ -17,13 +19,11 @@ def perform_additivity_test(analyses_in):
     genes = set(analyses.gene)
     ncs = set(analyses.index.get_level_values(1))
     idx = pd.IndexSlice
-    # print(nc)
 
     grouped = analyses.groupby(by=['gene', 'nc', 'construct'])
 
     # Evaluate additivity
     for quantity in quantities:
-        # quantity = quantities[2]
         calc = pd.DataFrame()
 
         # A special condition for alpha since we don't have the distribution
@@ -36,11 +36,7 @@ def perform_additivity_test(analyses_in):
             calc['means'] = grouped[quantity].first()
             calc['vars'] = grouped[quantity + 'V'].first()
 
-        # print(calc)
-        # print(grouped[quantity].count())
         calc['n'] = grouped[quantity].count()
-
-        # print(calc)
 
         # Add sum of constructs
         for col in ['t', 'nu']:
@@ -52,23 +48,8 @@ def perform_additivity_test(analyses_in):
                 no_pr = calc.loc[(gene, nc, 'no_pr'), :]
                 no_sh = calc.loc[(gene, nc, 'no_sh'), :]
 
-                # calc.loc[(gene, nc, 'sum'), ['means', 'vars', 'n']] = [no_pr.means + no_sh.means,
-                #                                                        no_pr.vars + no_sh.vars,
-                #                                                        no_pr.n]
-                # sum = calc.loc[(gene, nc, 'sum'), :]
-
                 p, t, nu = welchs_test(x1Mean=no_sh.means, x1V=no_sh.vars,
                                        n1=no_sh.n, x2Mean=bac.means, x2V=bac.vars, n2=bac.n)
-
-                # # # Test statistics
-                # t = (no_sh.means - bac.means) / \
-                #     np.sqrt(no_sh.vars / no_sh.n + bac.vars / bac.n)
-                # nu = ((no_sh.vars / no_sh.n + bac.vars / bac.n)**2
-                #       / (no_sh.vars**2 / no_sh.n / (no_sh.n - 1) + bac.vars**2 / bac.n / (bac.n - 1)))
-                # if not np.isnan(t):
-                #     p = 2 * (student.cdf(-np.abs(t), df=nu))
-                # else:
-                #     p = np.nan
 
                 calc.loc[(gene, nc, 'bac'), ['t']] = t
                 calc.loc[(gene, nc, 'bac'), ['nu']] = nu
@@ -90,36 +71,6 @@ def perform_additivity_test(analyses_in):
                     gene_filter = analyses.gene == gene
                     analyses.loc[idx[gene_filter, nc], label] = p
 
-    # # %% Independently, evaluate p-values of whether the observed limit on alpha/k can be described by the abortive l-TASEP model in nc13
-    #
-    # grouped = analyses.loc[idx[analyses.construct == 'bac', 13], :].groupby('gene')
-    # nc13_tau_n = grouped.JoK.count()
-    # nc13_tau = grouped.first()[['JoK', 'tau', 'tauV']]
-    #
-    # # nc13_n =
-    # # print([tau_rev, tau_revV, tau_rev_n])
-    #
-    # # Test statistics
-    # t = (nc13_tau['tau'] - tau_rev) / \
-    #     np.sqrt(nc13_tau['tauV'] / nc13_tau_n + tau_revV / tau_rev_n)
-    # nu = ((nc13_tau['tauV'] / nc13_tau_n + tau_revV / tau_rev_n)**2
-    #       / (nc13_tau['tauV']**2 / nc13_tau_n / (nc13_tau_n - 1) + tau_revV**2 / tau_rev_n / (tau_rev_n - 1))
-    #       )
-    #
-    # # Copy the p-value to the analysis table
-    # for gene in genes:
-    #     filter = (analyses.gene == gene) & (analyses.construct == 'bac')
-    #     label = 'abortive_theory_p_value'
-    #     # print(t.loc(axis=0)[gene])
-    #     p = 2 * (student.cdf(-np.abs(t.loc(axis=0)[gene]), df=nu.loc(axis=0)[gene]))
-    #     # print(p)
-    #     analyses.loc[idx[filter, 13], label] = p
-
-    # print(t, nu, p)
-
-    # print(nc13_tau, nc13_tau_n)
-
-    # print(calc)
     return analyses
 
 
