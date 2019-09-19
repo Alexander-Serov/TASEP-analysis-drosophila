308b0669c92dc56a813cff31523575504b9ed93d
Alexander Serov <asserov@gmail.com>
2019-09-19 10:56:47 +0200

    Cleaned up filtering by AP

--- filter_by_AP.py
+++ filter_by_AP.py
@@ -1,9 +1,12 @@
 """
-For hb, the filtering is performed simply by the location along the AP position, the max. expression region.
+This file contains the code used for filtering fluorescence traces by their AP positions
 
-For sn, no filtering for the moment.
+For hb, we find the AP corresponding to maximum expression APmax, and then take all traces in the interval [0.05; APmax - 0.05].
 
-For kn, the problem is that the expressing AP region moves with time. So instead of doing simple filtering, we re-evalute the expression median for each frame indpendently, and take the region +- 0.02 AP around it. I don't need to maintain connectivity of the traces since they are averaged out anyway later.
+For kn, the expressing AP region moves with time. So instead of doing filtering over the whole trace altogether, we do frame by frame filtering. We do not need the connectivity of the traces, because they are anyway averaged out in a single trace for each data set.
+The kn expressing region is rather small, so the filtering works by finding the median AP (mAP) of all expressing nuclei for each individual frame and then keeping only traces withing mAP +- 0.02.
+
+For sn, no filtering is performed.
 """
 
 
@@ -12,33 +15,30 @@ from tqdm import tqdm
 
 # Constants
 knirps_half_width_AP = 0.02
+hb_AP_margin = 0.05
 
 
 def filter_by_AP(data_in):
-    # data = data_in.copy()
-
-    # data.groupby[by = 'dataset_id']
     datasets = set(data_in.dataset)
     data = pd.DataFrame()
 
     for dataset in tqdm(datasets, desc='Parsing data sets'):
+        # Take one data set
         dataset_data = data_in[data_in.dataset == dataset]
         gene = dataset_data.gene.iloc[0]
-        # median_AP = dataset_data.ap_mean.median()
 
         if gene == 'hb':
-            # do not include the transition region of width 0.05 at the right side
+            # Localize the maximum and keep the AP region described above
             max_AP = dataset_data.ap_mean.max()
-            AP_trans_region_width = 0.05
-            filtered_data = dataset_data[(dataset_data.ap_mean <= max_AP - AP_trans_region_width)
-                                         & (dataset_data.ap_mean >= 0.05)]
+            filtered_data = dataset_data[
+                (dataset_data.ap_mean <= max_AP - hb_AP_margin) &
+                (dataset_data.ap_mean >= hb_AP_margin)]
             data = pd.concat([data, filtered_data])
 
         elif gene == 'kn':
             frames = set(dataset_data.frame)
-            # print(median_AP)
-            for frame in frames:  # tqdm(frames, desc='Analyzing frames'):
-                # AP_central_region_width = 0.04
+            # Individually process each frame
+            for frame in frames:
                 frame_data = dataset_data[dataset_data.frame == frame]
                 median_AP = frame_data.ap.median()
                 filtered_data = frame_data[
@@ -47,8 +47,6 @@ def filter_by_AP(data_in):
                 data = pd.concat([data, filtered_data])
 
         elif gene == 'sn':
-            # No filtering for the moment. Update if necessary
-            # AP_central_region_width = 0.04
             filtered_data = dataset_data
             data = pd.concat([data, filtered_data])
     print('Data filtered!')
