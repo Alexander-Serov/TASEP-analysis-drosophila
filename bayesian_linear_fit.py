67576291003a0cd12b5fd04a2f350028779ef689
Alexander Serov <asserov@gmail.com>
2019-09-19 14:38:48 +0200

    Clean up the current-density diagram fit

--- bayesian_linear_fit.py
+++ bayesian_linear_fit.py
@@ -1,12 +1,19 @@
 """
-Perform a Bayesian linear fit.
-If c = False, the line must pass through the origin
-See D'Agostini2005 for further details of the method.
+Perform a Bayesian linear fit for a heteroscedastic set of points with uncertainties along both axes.
+See the accompanying article and D'Agostini2005 for further details of the method.
 
-If the fit converges to sigmaV = 0, that means that the supplied variance of the data can already explain all the variance observed. No need to attach variance to the equaiton.
+The method allows not only to attach uncertainties to the data points, but also to the functional relation itself.
+Basically, this means that the relation is a (normal) distribution around its mean rather than a deterministic function relating x and y.
+If the fit converges to sigmaV = 0, this means that the supplied variance of the data can already explain all the variance observed. No need to attach variance to the equaiton.
 
-Input:
-prior(m, sigmaV) or prior(m, c, sigmaV) - by default is a uniform prior on all vars. Does not need to be proper
+By default, we use a flat prior for sigmaV and intersect, and a uniform angle prior for the slope.
+A custom prior function can be supplied as a parameter
+
+Parameters:
+x, y    -   float, array-like, lists of coordinates of 2D data points
+Vx, Vy  -   float, array-like, the corresponding of each data point
+c       -   boolean, whether the algorithm should look for a non-zero intersect. Set c = False to make the fit pass through the origin
+prior(m, sigmaV) or prior(m, c, sigmaV) - a prior function. Does not need to be proper
 
 Return:
 m, mV - line slope and its variance
@@ -17,35 +24,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize as opt
 
-atol = 1e-16
+# Constants
+plot = False
 
 
 def bayesian_linear_fit(x, y, Vx, Vy, c=True, prior=None):
-    plot = 0
     sigmaV_guess = 0
     m_guess = 1
-    # print(x, y)
 
     if c:
+        # If not passing through the origin
         phi = posterior(x, y, Vx, Vy, c=True, prior=prior)
-        guess = (m_guess, 0, sigmaV_guess)  # (m, c, sigmaV)
-        min = opt.minimize(phi, guess)  # , bounds=((None, None), (None, None), (0, None)))
+        guess = (m_guess, 0, sigmaV_guess)
+        min = opt.minimize(phi, guess)
         m_est, c_est, sigmaV_est = min.x
+
+        # Calculate the uncertainty on the estimates by calculating the Hessian inverse
         mV_est, cV_est, sigmaVV_est = min.hess_inv.diagonal()
 
     else:
+        # If passing through the origin
         phi = posterior(x, y, Vx, Vy, c=False, prior=prior)
-        guess = (m_guess, sigmaV_guess)  # (m, sigmaV)
-        min = opt.minimize(phi, guess)  # , bounds=((None, None), (0, None)))
+        guess = (m_guess, sigmaV_guess)
+        min = opt.minimize(phi, guess)
         m_est, sigmaV_est = min.x
         mV_est, sigmaVV_est = min.hess_inv.diagonal()
 
         c_est = 0
         cV_est = 0
-    # Find the minimum of phi (max. likelihood)
-
-    # print(min)
-    # print(np.sqrt(min.hess_inv.todense()))
 
     if plot:
         plt.figure(clear=True, num=1)
@@ -60,9 +66,6 @@ def bayesian_linear_fit(x, y, Vx, Vy, c=True, prior=None):
     # Construct the result dictionary
     estimates = {'m': m_est, 'mV': mV_est, 'c': c_est,
                  'cV': cV_est, 'sigmaV': sigmaV_est, 'sigmaVV': sigmaVV_est}
-    # print(estimates)
-    # if c:
-    #     res['c', 'cV'] = [c_est, cV_est]
 
     return estimates
 
@@ -70,19 +73,16 @@ def bayesian_linear_fit(x, y, Vx, Vy, c=True, prior=None):
 def posterior(x, y, Vx, Vy, c=True, prior=None):
     """
     Returns phi = -ln(posterior), non-normalized as a function of the control parameters (m, c, Vv)
-    Using a flat prior for sigmaV and intersect, and a uniform angle prior for the slope.
+    By default, we use a flat prior for sigmaV and intersect, and a uniform angle prior for the slope.
 
     posterior = p(m, c, sigmaV | x, y, Vx, Vy)
     """
 
-    # Identify and drop nans
+    # Drop nans
     inds_not_nan = list(set(np.flatnonzero(~np.isnan(x * y))))
     x, y, Vx, Vy = [ar[inds_not_nan] for ar in [x, y, Vx, Vy]]
-    # # Concatenate the prior with the data set
-    # x, y, Vx, Vy = [np.append(ar[inds_not_nan], el)
-    #                 for ar, el in zip([x, y, Vx, Vy], [xp, yp, Vpx, Vpy])]
 
-    # Default uniform prior
+    # Default prior
     if not prior:
         if c:
             def prior(m, c, sigmaV):
--- calculate_free_travel_time.py
+++ calculate_free_travel_time.py
@@ -2,58 +2,64 @@ import numpy as np
 
 from bayesian_linear_fit import bayesian_linear_fit
 from constants import LV, L, k, kV
-from least_squares_BCES import least_squares_BCES
 
 
 def calculate_free_travel_time(analyses_in):
-    """Estimate the free passage time by fitting the non-scaled current-density diagram across all ncs for each gene and construct individually
+    """
+    Estimate the free passage time by fitting the non-scaled current-density diagram across all ncs for each gene and construct individually.
+
+    The fitting procedure takes into account uncertainties in data points along both axes.
+    the fit line must pass through 0.
+
+    A Gaussian distribution of the slope values is used a prior.
+    The width of the distribution `TV_prior` is defined by the a priori error in the elongation rate k and effective gene length L.
+    Those are set in `constants.py`
     """
 
     analyses = analyses_in.copy()
-    genes = set(analyses.gene.values)
-    constructs = set(analyses.construct.values)
-    ncs = range(11, 15)
+    genes = set(analyses.gene)
+
+    # Add columns to the analyses table
     columns = ['T', 'TV', 'L', 'LV']
     for column in columns:
         analyses[column] = np.nan
 
-    # LV = 5e3 ** 2
-
-    # Estimate the prior on the travel time
+    # Make a prior on the travel time
     T_prior = L / k
     TV_prior = T_prior**2 * (LV / L**2 + kV / k**2)
 
     def prior_func(m, sigmaV):
+        """
+        The prior must be a function of 2 parameters, so sigmaV in the definition is necessary.
+        But this particular prior does not depend on sigmaV
+        This is a conjugated prior in the form of just the Gaussian distribution for the slope.
+        """
         return np.exp(-(m - T_prior)**2 / 2 / TV_prior) * (2 * np.pi * TV_prior)**(-1 / 2)
 
-    # Group data by gene because must be same length for all constructs
-    for gene_id, gene in enumerate(genes):
-        # for construct_id, construct in enumerate(constructs):
+    # Fit the diagram individually for each gene, but all constructs and ncs mixed
+    # This makes sense because all constructs and nc supposedly have the same length and polymerase elongation rate
+    for gene in genes:
         analyses_filtered = analyses[(analyses.gene == gene)]
         indices = analyses_filtered.index
 
-        Ns, Ns_V, slopes, slopes_V = [], [], [], []
-        # for nc in ncs:
-        # Ns = np.concatenate([Ns, analyses_filtered['max' + str(nc)].values])
         Ns = analyses_filtered['max'].values
         NsV = analyses_filtered['maxV'].values
         slopes = analyses_filtered['slope'].values
         slopesV = analyses_filtered['slopeV'].values
 
         # Fit N/s passing through zero. Give result in minutes
-        fit = bayesian_linear_fit(x=slopes, Vx=slopesV, y=Ns, Vy=NsV, c=0, prior=prior_func)
+        fit = bayesian_linear_fit(x=slopes, Vx=slopesV, y=Ns, Vy=NsV, c=False, prior=prior_func)
         T, TV = [fit[key] for key in ['m', 'mV']]
 
         # Estimate L from T and k
         L_est = k * T
         LVest = T**2 * kV + k**2 * TV
-        # print(np.sqrt([kV / k**2, TV / T**2]))
 
+        # Round up and print
         L_rnd, Lstd_rnd = np.round([L_est, np.sqrt(LVest)], decimals=-1)
         print(f'{gene}: T = {T:.2f} +- {np.sqrt(TV):.2f}, L = {L_rnd:.0f} +- {Lstd_rnd:.0f}')
-        # print(fit)
 
-        # Save
+        # Save to analyses
         analyses.loc[indices, 'T'] = T
         analyses.loc[indices, 'TV'] = TV
         analyses.loc[indices, 'L'] = L_est
